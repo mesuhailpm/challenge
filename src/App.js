9539c9c48cd12c25c9c3284bc7139445c1507c1f
suhailpm <tosuhailpm@gmail.com>
2023-10-03 13:08:45 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import Navbar from './components/Navbar.jsx';
-import './App.css';
+import Navbar from "./components/Navbar.jsx";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <main>
-        Here comes other part
-      </main>
+      <main className="text-start mt-2 p-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+        dolorem suscipit illo quibusdam commodi cupiditate minus sit molestias
+        molestiae iusto, magni deserunt eveniet officiis, facere aperiam
+        explicabo quaerat eligendi iste omnis unde, a rem natus. Pariatur vero
+        ab quod quae voluptates ratione, inventore officiis, possimus
+        voluptatibus atque id facilis optio?
+        <br />
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quo neque
+        in veniam et reprehenderit unde numquam tempore dolore perspiciatis.
 
+      </main>
     </div>
   );
 }
